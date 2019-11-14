e2dc6800b06735c2ab35b7ca3ca039089f205d5d
Per Christian Kvalvik <pc@kvalvik.no>
2019-11-14 16:41:49 +0100

    Implement silent view model

--- app/src/main/java/me/pckv/kompisapp/ui/listing/view/ListingViewModel.java
+++ app/src/main/java/me/pckv/kompisapp/ui/listing/view/ListingViewModel.java
@@ -1,7 +1,11 @@
 package me.pckv.kompisapp.ui.listing.view;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
 import androidx.lifecycle.ViewModel;
 
+import me.pckv.kompisapp.data.DatalessResult;
 import me.pckv.kompisapp.data.ListingRepository;
 
 public class ListingViewModel extends ViewModel {
@@ -12,5 +16,45 @@ public class ListingViewModel extends ViewModel {
     public ListingViewModel(ListingRepository listingRepository, long listingId) {
         this.listingRepository = listingRepository;
         this.listingId = listingId;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void actiavteListing() {
+        new AsyncTask<Void, Void, DatalessResult>() {
+            @Override
+            protected DatalessResult doInBackground(Void... voids) {
+                return listingRepository.activateListing(listingId);
+            }
+        };
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void deactiavteListing() {
+        new AsyncTask<Void, Void, DatalessResult>() {
+            @Override
+            protected DatalessResult doInBackground(Void... voids) {
+                return listingRepository.deactivateListing(listingId);
+            }
+        };
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void assignListing() {
+        new AsyncTask<Void, Void, DatalessResult>() {
+            @Override
+            protected DatalessResult doInBackground(Void... voids) {
+                return listingRepository.assignListing(listingId);
+            }
+        };
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void unassignListing() {
+        new AsyncTask<Void, Void, DatalessResult>() {
+            @Override
+            protected DatalessResult doInBackground(Void... voids) {
+                return listingRepository.unassignListing(listingId);
+            }
+        };
     }
 }
