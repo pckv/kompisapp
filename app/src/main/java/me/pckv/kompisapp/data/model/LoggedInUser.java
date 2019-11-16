7f451f215ef6f874deaa3edd4fc23e3dca11a4b9
Per Christian Kvalvik <pc@kvalvik.no>
2019-11-16 15:24:01 +0100

    Make LoggedInUser subclass of User

--- app/src/main/java/me/pckv/kompisapp/data/model/LoggedInUser.java
+++ app/src/main/java/me/pckv/kompisapp/data/model/LoggedInUser.java
@@ -2,18 +2,18 @@ package me.pckv.kompisapp.data.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
  * Data class that captures user information for logged in users retrieved from UsersRepository
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoggedInUser {
+public class LoggedInUser extends User {
 
     private String token;
-    private long id;
     private String email;
-    private String displayName;
 }
